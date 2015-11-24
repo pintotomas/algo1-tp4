285f0f7115bc41a712999b4a9297be0c2a39bda1
t0mixcs <tomasp834@gmail.com>
2015-11-24 18:41:47 -0300

    Create pila.py

---
+++ pila.py
@@ -0,0 +1,24 @@
+class PilaVaciaError(object):
+    pass
+class Pila(object):
+    """....."""
+    def __init__(self):
+        self.elementos = []
+
+    def apilar(self,dato):
+        self.elementos.append(dato)
+
+    def desapilar(self):
+       try:    
+           quitar_dato = self.elementos.pop()
+           return quitar_dato
+       except IndexError:
+           raise PilaVaciaError("La pila esta vacia")
+
+    def ver_tope(self):
+        try:
+            return self.elementos[-1]
+        except IndexError:
+            raise PilaVaciaError()
+    def esta_vacia(self):
+        return len(self.elementos) == 0
