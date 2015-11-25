4d0608f102d4e672c7237fb3eeabc47d012fa9a0
t0mixcs <tomasp834@gmail.com>
2015-11-24 22:46:46 -0300

    Update pila.py

--- pila.py
+++ pila.py
@@ -1,24 +1,50 @@
-class PilaVaciaError(Exception):
-    pass
-class Pila(object):
-    """....."""
-    def __init__(self):
-        self.elementos = []
+import argparse
+import pila
+import errores
+def cargar_codigo(archivo):
+    """ Pre: El archivo debe existir y se tiene que poder leer
+        Post: Devuelve una lista con los comandos que reconoce
+        un interprete de matacerebros
+    """
+    with open(archivo) as archivo_matacerebros:
+        comandos_matacerebro = "<>+-[]."
+        comandos_archivo = []
+        pila_saltos = pila.Pila()
+        pos_actual = 0
+        for linea in archivo_matacerebros:
+            for caracter in linea:
+                if caracter in comandos_matacerebro:
+                    if caracter == "[":
+                        pila_saltos.apilar([caracter,pos_actual])
+                        comandos_archivo.append([caracter,pos_actual])
+                        pos_actual+=1
+                        continue
+                    elif caracter == "]":
+                        try:
+                            if pila_saltos.ver_tope()[0]  == "[":
+                                posicion_anterior = pila_saltos.desapilar()[1]
+                                comandos_archivo[posicion_anterior][1] = pos_actual
+                                comandos_archivo.append([caracter,posicion_anterior])
+                                pos_actual+=1
+                                continue
+                        except pila.PilaVaciaError:
+                            raise errores.MCSyntaxError
+                    comandos_archivo.append(caracter)
+                    pos_actual+=1
+    if pila_saltos.esta_vacia():
+        return comandos_archivo
+    else: raise MCSyntaxError
+#Este codigo devuelve la lista de cada comando pero en cada corchete "[" o "]" guarda la posicion del corchete asociado            
+def main():
+    parser = argparse.ArgumentParser(description='Interprete de codigo MataCerebros')
+    parser.add_argument('archivo', metavar='archivo', help='archivo con codigo a interpretar')
+    parser.add_argument('-d', '--debug', action='store_true', help='modo debug')
+    args = parser.parse_args()
 
-    def apilar(self,dato):
-        self.elementos.append(dato)
+    nombre_archivo = args.archivo #Esto es un string, que vendria a tener 
+    modo_debug = args.debug #si en la linea de comandos le pasas aparte del nombre, "-d", esto guarda True
 
-    def desapilar(self):
-       try:    
-           quitar_dato = self.elementos.pop()
-           return quitar_dato
-       except IndexError:
-           raise PilaVaciaError("La pila esta vacia")
-
-    def ver_tope(self):
-        try:
-            return self.elementos[-1]
-        except IndexError:
-            raise PilaVaciaError()
-    def esta_vacia(self):
-        return len(self.elementos) == 0
+    lista_comandos = cargar_codigo(nombre_archivo)
+    
+    
+main()
